ed26b68779971d85843ad20e3760b44d01a0027a
cyrus sarwale <cyrus.sarwale@misterspex.de>
2020-12-29 05:11:15 +0100

    break down Homepage component to small pieces

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.sass";
+import Directory from "../../components/directory/Directory";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default Homepage;
